fce6d984c55f189c74773efe4b4e0691ecb2098f
Dat Nguyen <tandat198@gmail.com>
2019-11-25 09:38:05 +0700

    new update

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let projects = document.getElementsByClassName("projects");
+let button = document.getElementsByClassName("fa-angle-up")[0];
 for (let i=0; i<projects.length; i++) {
 projects[i].addEventListener("mouseenter",function(event) {
 		let name = event.target.children[1].innerHTML;
@@ -9,3 +10,16 @@ projects[i].addEventListener("mouseenter",function(event) {
 	})
 	})
 }
+let bottom = 0;
+function updown() {
+
+	if(bottom<15) {
+		bottom ++;
+		button.style.bottom = bottom + "px";
+	}
+	else{
+		bottom=0;
+	}
+
+}
+setInterval(updown,80);
